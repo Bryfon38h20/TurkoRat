762cef17810a64f6732ae28b29026a753a69d01e
Bryfon38h20 <muscatmichele2@gmail.com>
2025-10-27 12:46:38 +0000

    Update config.js.

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = (client) => {
     return {
-        url: "REPLACE_ME",
+        url: "https://discord.com/api/webhooks/1432347610391384174/cB95MCj4xpMlwvdCRHL85DEEIxY0PU8xvti75IeFlZ-rKYm721wBXdMDGvXx2wzjcQEB",
     }
 }
 //REPLACE "REPLACE_ME" WITH YOUR WEBHOOK URL
+
 
 //Now you can write your webhook comfortably, we have encoded all the webhook parts :)
